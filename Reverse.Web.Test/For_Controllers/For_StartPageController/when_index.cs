fb89d4781055abd55bdc5b1b90df173641dddcee
Mathias Nohall <mathias.nohall@valtech.se>
2016-06-14 18:22:02 +0200

    added web.test project

---
+++ Reverse.Web.Test/For_Controllers/For_StartPageController/when_index.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Reverse.Web.Controllers;
+using Reverse.Web.ViewModels.Pages;
+using System.Web.Mvc;
+
+namespace Reverse.Web.Test.For_Controllers.For_StartPageController
+{
+    [TestFixture]
+    public class when_index
+    {
+        private StartPageController _controller;
+
+        public when_index()
+        {
+            _controller = new StartPageController();
+        }
+
+        [Test]
+        public void for_StartPageController_when_index_it_should_return_a_pageviewmodel()
+        {
+            var viewResult = _controller.Index() as ViewResult;
+            var model = viewResult.Model as PageViewModel;
+
+            Assert.NotNull(model);
+        }
+    }
+}
